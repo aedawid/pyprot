4ad62fcf692c90bb12955ac7b083d5ca03c4c6ee
Aleksandra Badaczewska <adawid@ac02yf0j3jwf1.chem.iastate.edu>
2019-09-25 15:26:20 -0500

    tests

---
+++ test.py
@@ -0,0 +1,9 @@
+#import manage_pdb
+from manage_pdb import read_pdb, read_pdb_model, write_pdb, write_pdb_model
+
+structure = []
+#read_pdb('1D3Z_UB_NMR.pdb', structure)
+atoms = [' N  ', ' CA ']
+read_pdb_model('1D3Z_UB_NMR.pdb', '3', 'A', atoms, structure)
+write_pdb(structure)
+#write_pdb_model(structure, 11)
