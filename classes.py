f8715143ede72f03829566222343619b5a698d4b
Aleksandra Badaczewska <adawid@ac02yf0j3jwf1.chem.iastate.edu>
2019-10-01 16:13:38 -0500

    few new self.functions() for PDB class

--- classes.py
+++ classes.py
@@ -5,6 +5,7 @@ class Pdb(object):
     def __init__(self, structure):
 
         self.structure = structure
+        self.header = header
     
     
     def write_pdb(self, which = 1):
@@ -28,15 +29,93 @@ class Pdb(object):
             print("%-6s%5s %4s %3s %s%4s    %8s%8s%8s%6s%6s           %3s"%tuple(atom))
         print("ENDMDL")
     
-    def seq(self):
+    def seq_from_struct(self):
         fasta = ''
         for atom in self.structure[0]:
             if atom[2] == ' CA ':
                 fasta += AA_code(atom[3])
         return fasta
     
-    def __select(self, which):
+    def seq_from_header(self):
+        fasta = ''
+        for row in self.header['SEQRES']:
+            tokens = row.split()
+            for t in tokens:
+                if len(t) == 3 and t.isalpha():
+                    fasta += AA_code(t)
+        return fasta
+    
+    def ss_for_struct(self):
+        seq = self.seq_from_struct()
+        ids = self.resi_ids()
+        ch_r = self.chain_ranges()
+        ss = 'C'*len(seq)
+        s = list(ss)
 
+        def parse_row(row, n, c):
+            tokens = row.split()
+            if tokens[2+n] == tokens[5+n]:
+                for ch in ch_r:
+                    if tokens[2+n] == ch[2]:
+                        start = int(ch[0])
+                        ss_first = start + int(tokens[3+n]) - int(ids[start])
+                        ss_last = ss_first + int(tokens[6+n]) - int(tokens[3+n])
+                        if seq[ss_first] == AA_code(tokens[1+n]):
+                            for i in range(ss_first, ss_last + 1):
+                                s[i] = c
+
+        if len(seq) != len(ids):
+            print("ERROR: len(seq) != len(ids)")
+            print(len(seq), len(ids))
+        else:
+            for row in self.header['HELIX']:
+                parse_row(row, 0, 'H')
+            for row in self.header['SHEET']:
+                parse_row(row, 1, 'E')
+
+        ss = "".join(s)
+        return ss
+    
+    def ss_for_seq(self, c = 'X'):
+        seq_struct = self.seq_from_structure()
+        seq_full = self.seq_from_header()
+        ss_struct = self.ss_for_struct()
+        if len(seq_struct) == len(seq_full):
+            ss_full = ss_struct
+        else:
+            ss_full = 'C'*len(seq_full)
+            s = list(ss)
+            ids = self.resi_ids()
+            missing = self.missing_resi()
+            ch_r = self.chain_ranges()
+            first = 0
+            if int(missing[0][2]) < int(ids[0]):
+                first = int(missing[0][2])
+            else:
+                first = int(ids[0])
+            for m in missing:
+                for ch in ch_r:
+                    if m[1] == ch[2]:
+                        start = int(ch[0])
+                    
+
+    
+    def compare_seqs(self):
+        seq_struct = list(self.seq_from_structure())
+        seq_full = list(self.seq_from_header())
+        if len(seq_struct) != len(seq_full):
+            for aa in seq_full:
+                
+    
+    def missing_resi(self):
+        missing = []
+        for row in self.header['REMARK 465']:
+            tokens = row.split()
+            if len(tokens) == 3:
+                missing.append((AA_code(tokens[0]), tokens[1], tokens[2]))
+        return missing
+    
+    def __select(self, which):
         vec = []
         for atom in self.structure[0]:
             vec.append(atom[which])
@@ -56,6 +135,25 @@ class Pdb(object):
     
     def chain_ids(self):
         return self.__select(4)
+    
+    def chain_ranges(self):
+        ch_ids = self.chain_ids()
+        ranges = []
+        if ch_ids[0] == ch_ids[len(ch_ids)-1]:
+            ranges.append((0, len(ch_ids)-1))
+        else:
+            ch = ch_ids[0]
+            first = 0
+            n = 0
+            for i in ch_ids:
+                if ch != i:
+                    ranges.append((first, n-1, ch))
+                    first = n
+                    ch = i
+                elif n == len(ch_ids)-1:
+                    ranges.append((first, n, ch))
+                n += 1
+        return ranges
     
     def resi_ids(self):
         return self.__select(5)
@@ -122,7 +220,7 @@ class AA(object):
         self.z = 0.0
 
 
-def read_pdb(filename, w_model = '0', w_chain = '0', w_atoms = []):
+def read_pdb(filename, w_model = '0', w_chain = '0', w_atoms = [], alter = 'A'):
     
     def parse_line(line, model):
 
@@ -135,36 +233,49 @@ def read_pdb(filename, w_model = '0', w_chain = '0', w_atoms = []):
             else:
                 for at in w_atoms:                 ###parse atoms
                     if line[12:16] == at:
-                        model.append(atom)
+                        if line[16] == ' ' or line[16] == alter:
+                            model.append(atom)
         elif line[21] == w_chain:                  ##parse single chain
             if not len(w_atoms):
                 model.append(atom)
             else:
                 for at in w_atoms:
                     if line[12:16] == at:
-                        model.append(atom)
+                        if line[16] == ' ' or line[16] == alter:
+                            model.append(atom)
+    
+    def parse_header(line):
+        for key in header:
+            if line.startswith(key):
+                header[key].append(line[11:80])
     
     model = []
     structure = []
     with open(filename, 'r') as pdb:
         if w_model == '0':                                 #parse all_models
             for line in pdb:
-                if line[:4] == 'ATOM' or line[:6] == "HETATM":
+                if line[:4] == 'ATOM':# or line[:6] == "HETATM":
                     parse_line(line, model)
                 elif line.startswith('ENDMDL'):
                     structure.append(model)
                     model = []
-        else:
+                else:
+                    parse_header(line)
+            if not len(structure):
+                structure.append(model)
+        else:                                              #parse single model
             is_ok = 'false'
             for line in pdb:
                 if is_ok == 'true':
-                    if line[:4] == 'ATOM' or line[:6] == "HETATM":
+                    if line[:4] == 'ATOM':# or line[:6] == "HETATM":
                         parse_line(line, model)
                     elif line.startswith('ENDMDL'):
                         structure.append(model)
                         break
-                elif line.startswith("MODEL%9s"%w_model):  #parse single model
+                elif line.startswith("MODEL%9s"%w_model):
                     is_ok = 'true'
+                else:
+                    parse_header(line)
     return structure
 
 
@@ -208,6 +319,8 @@ aa_names = {
     'S': 'SER', 'T': 'THR', 'V': 'VAL', 'W': 'TRP', 'Y': 'TYR'
 }
 
+
+
 ##### 0 - id, 1 - weight, 2 - frequency, 3 - charge, 4 - polar, 5 - aromatic, 6 - hp_KD
 aa_features = {
     'I': ( 0, 131.175, 5.49,  0.0, 0.0, 0.0,  4.5 ),
@@ -230,4 +343,23 @@ aa_features = {
     'N': (17, 132.119, 3.93,  0.0, 1.0, 0.0, -3.5 ),
     'K': (18, 146.189, 5.19,  1.0, 1.0, 0.0, -3.9 ),
     'R': (19, 174.203, 5.78,  1.0, 1.0, 0.0, -4.5 )
+}
+
+header = {
+    'HEADER'    : [],
+    'TITLE'     : [],
+    'COMPND'    : [],
+    'SOURCE'    : [],
+    'KEYWDS'    : [],
+    'EXPDTA'    : [],
+    'AUTHOR'    : [],
+    'JRNL'      : [],
+    'REMARK   2': [],
+    'REMARK 800': [],
+    'REMARK 465': [],
+    'DBREF'     : [],
+    'SEQRES'    : [],
+    'HELIX'     : [],
+    'SHEET'     : [],
+    'CISPEP'    : []
 }
