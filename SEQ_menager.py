9ba95e8ecf7a15bb8cc32684c442699783976b7c
Aleksandra Badaczewska <adawid@ac02yf0j3jwf1.chem.iastate.edu>
2019-10-16 16:28:57 -0500

    Adding read_LLPSDB(key) method to SEQ_menager module.

--- SEQ_menager.py
+++ SEQ_menager.py
@@ -1,7 +1,8 @@
+import os
 from PDB_menager import AA_ATTRIBUTES
 
 def read_fasta(filename):
-    """Read fasta format file. Return string type seq."""
+    """Reads fasta format file. Return string type seq."""
     fasta = ''
     with open(filename, 'r') as pdb:
         for line in pdb:
@@ -11,7 +12,7 @@ def read_fasta(filename):
 
 
 def read_conservation(clustal_aln, skylign):
-    """Read matrix 21 cols x N rows, eg. skylign format file. Return conservation values for each position in query sequence."""
+    """Reads matrix 21 cols x N rows, eg. skylign format file. Return conservation values for each position in query sequence."""
     conservation = []
     seq = ''
     try:
@@ -32,3 +33,36 @@ def read_conservation(clustal_aln, skylign):
                     conservation.append((seq[n], tokens[AA_ATTRIBUTES[seq[n]][0]]))
                 n += 1
     return conservation
+
+
+def read_LLPSDB(key):
+    """Reads subset of LLPSDB database. @key is a subset name: DESIG, NATUR, C_ONE, C_TWO, C_MOR, P_DNA, P_RNA, P_PRT"""
+    path="/Users/adawid/PROJECTS/databases/LLPSDB/"
+    data=['/5_Uniprot_id', '/0_PID', '/14_DisProt_ID', '/7_Species', '/11_Sequence', '/12_IDR', '/13_LCR']
+    vec=[]
+    r=''
+    for n1, feature in enumerate(data):
+        try:
+            with open(path+key+feature, "r") as f:
+                for n2, row in enumerate(f):
+                    if n1 == 0:
+                        vec.append([row.strip()])
+                    elif n1 == 4:
+                        for i in row.strip().split(';'):
+                            if i.isalpha():
+                                r += i+" "
+                        vec[n2].append(r)
+                        r=''
+                    else:
+                        vec[n2].append(row.strip())
+        except:
+            print("ERROR: File %s can't be open!"%str(path+key+feature))
+    try:
+        with open(path+key+"/1_P_name", "r") as names:
+            for n, name in enumerate(names):
+                LLPSDB[name.strip()] = vec[n]
+    except:
+        print("ERROR: File %s can't be open!"%str(path+key+'/1_P_name'))
+    return LLPSDB
+
+LLPSDB={}
---
+++ ex_SEQ_menager.py
@@ -0,0 +1,6 @@
+import sys
+from SEQ_menager import read_LLPSDB
+
+
+db = read_LLPSDB('P_PRT')
+print(db)
